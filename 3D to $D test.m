554dd7a58ec220b3850577fa2f23664440463772
romasalah <roma.salah@gmail.com>
2018-10-12 21:57:35 +0200

    Delete 3D to $D test.m

--- 3D to $D test.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'/Users/NEMO_admin/Desktop/Omar/Lib/3D to $D test_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
